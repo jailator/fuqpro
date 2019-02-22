3150b980f10cd54cd4cc2bdf610132a107e6d9fc
jailator <jailator@hotmail.com>
2019-02-22 14:16:57 +0700

    new upload

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<style>
+div.made-with {
+    margin: auto; 
+     padding: 10px;}
+
+</style>
+<footer>
+    <div class="footer-content container">
+        <div class="made-with" style="color:#FCDE43;">Guitar Shop | by fuqOff Group</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
